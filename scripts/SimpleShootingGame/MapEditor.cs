be4abf3bf0152c4a4f5af7ac5ca895d8a2c5bdf1
Michael Titov <80213389+MishaTitov@users.noreply.github.com>
2023-02-11 22:15:57 +0200

    Add files via upload

---
+++ scripts/SimpleShootingGame/MapEditor.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(MapGenerator))]
+public class MapEditor : Editor
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void OnInspectorGUI()
+    {
+        MapGenerator map = target as MapGenerator;
+        if (DrawDefaultInspector())
+        {
+            map.GenerateMap();
+        }
+
+        if (GUILayout.Button("Generate Map"))
+        {
+            map.GenerateMap();
+        }
+    }
+}
